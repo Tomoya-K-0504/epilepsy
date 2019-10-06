51dd581f91693c2a369790dd5a48f54cd1531336
Tomoya Koike <makeffort134@gmail.com>
2019-10-06 11:50:07 +0900

    k-fold available

--- src/experiment.py
+++ src/experiment.py
@@ -7,6 +7,7 @@ from ml.models.model_manager import model_manager_args, BaseModelManager
 from eeglibrary.src.eeg_dataset import EEGDataSet
 from eeglibrary.src.preprocessor import preprocess_args
 from eeglibrary import eeg
+from ml.tasks.train_manager import TrainManager, train_manager_args
 import torch
 
 
@@ -14,11 +15,8 @@ LABELS = {'null': 0, 'seiz': 1, 'bckg': 2}
 
 
 def train_args(parser):
-    train_parser = parser.add_argument_group('train arguments')
-    train_parser.add_argument('--only-model-test', action='store_true', help='Load learned model and not training')
-    train_parser.add_argument('--test', action='store_true', help='Do testing')
+    parser = train_manager_args(parser)
     parser = preprocess_args(parser)
-    parser = model_manager_args(parser)
 
     return parser
 
@@ -31,40 +29,23 @@ def load_func(path):
     return torch.from_numpy(eeg.load_pkl(path).values.reshape(-1, ))
 
 
-def train(train_conf) -> float:
+def experiment(train_conf) -> float:
 
-    phases = ['train', 'val', 'test']
+    dataset_cls = EEGDataSet
+    set_dataloader_func = set_dataloader
 
-    class_names = list(set(LABELS.values()))
     metrics = [
         Metric('loss', direction='minimize'),
-        Metric('accuracy', direction='maximize'),
-        Metric('recall_1', direction='maximize', save_model=True),
+        Metric('accuracy', direction='maximize', save_model=True),
+        Metric('recall_1', direction='maximize'),
         Metric('recall_2', direction='maximize'),
         Metric('far', direction='minimize')
     ]
 
-    train_conf.update({
-        'class_names': list(range(len(class_names))),
-        'load_func': load_func,
-        'label_func': label_func,
-    })
+    train_conf['class_names'] = list(LABELS.values())
+    train_manager = TrainManager(train_conf, load_func, label_func, dataset_cls, set_dataloader_func, metrics)
 
-    # dataset, dataloaderの作成
-    dataloaders = {}
-    for phase in phases:
-        dataset = EEGDataSet(train_conf[f'{phase}_path'], train_conf)
-        dataloaders[phase] = set_dataloader(dataset, phase, train_conf)
-
-    # modelManagerをインスタンス化、trainの実行
-    model_manager = BaseModelManager(class_names, train_conf, dataloaders, metrics)
-
-    # モデルの学習を行う場合
-    if not train_conf['only_model_test']:
-        model_manager.train()
-
-    if train_conf['only_model_test'] or train_conf['test']:
-        model_manager.test()
+    train_manager.train()
 
     # if train_conf['train_manifest'] == 'all':
     #     for sub_name in subject_dir_names:
@@ -83,4 +64,4 @@ if __name__ == '__main__':
     assert train_conf['train_path'] != '' or train_conf['val_path'] != '', \
         'You need to select training, validation data file to training, validation in --train-path, --val-path argments'
     # returns loss or accuracy
-    train(train_conf)
+    experiment(train_conf)
